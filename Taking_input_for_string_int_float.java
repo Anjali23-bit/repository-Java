534bcc6a4bc51cf5e31243d917d26dc31a5a3ee2
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-08-08 11:56:01 +0530

    Taking_input_for_string_int_float.java

---
+++ Taking_input_for_string_int_float.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int a=sc.nextInt();
+        float b=sc.nextFloat();
+        System.out.println("Name is: "+name);
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+    }
+}
