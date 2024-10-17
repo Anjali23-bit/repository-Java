087efbbf8e4570bcbcbd0bc9ae726f7026fd7d0d
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-10-17 17:30:01 +0530

    Right angle triangle star pattern.java

---
+++ Right angle triangle star pattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j;
+        int n=1;
+        //int row=sc.nextInt();
+        int column=sc.nextInt();
+        for(i=0;i<column;i++)
+        {
+            for(j=1;j<i+1;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
