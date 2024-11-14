3d5b5f5fbc6392aa91190f93963e5bb19b319656
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-11-14 12:08:39 +0530

    Final keyword.java

---
+++ Final keyword.java
@@ -0,0 +1,12 @@
+public class Main {
+  final int speedlimit=90;
+  void run()
+  {
+    speedlimit=100;
+  }
+    public static void main(String[] args) {
+      
+      Main b=new Main();
+      b.run();
+  }
+}
