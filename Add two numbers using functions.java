9899189482a28c6ceeb47e37fd3d1ac2d71b13a4
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-11-14 11:59:26 +0530

    Add two numbers using functions.java

---
+++ Add two numbers using functions.java
@@ -0,0 +1,13 @@
+public class Main {
+  static int printAdd(int x,int y)
+  {
+    System.out.println("The sum is:"+(x+y));
+    return x+y;
+  }
+    public static void main(String[] args) {
+      
+      int a=10;
+      int b=20;
+      printAdd(a,b);
+  }
+}
